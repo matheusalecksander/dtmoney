a6067a1d2c57a57a31de05a5e98fadf8fb7ba847
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2021-12-01 20:50:37 -0300

    Criado a conexão com a api utilizando axios e criado modal de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleNewTransactionModalClose() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header
+        onNewTransactionModalOpen={handleNewTransactionModalOpen}
+      />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleNewTransactionModalClose}
+      />
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onNewTransactionModalOpen: () => void
+}
+
+export function Header({ onNewTransactionModalOpen }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onNewTransactionModalOpen}>
           Nova Transação
         </button>
       </Content>
